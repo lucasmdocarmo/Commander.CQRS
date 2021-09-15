b1645fa9b4cd2c60b7616241fc428a5a340821ec
Lucas Mariano do carmo <lucas.mariano.carmo@live.com>
2021-09-15 13:33:03 -0300

    Update - Events Tests

---
+++ Commander.Playground.Tests/Contexts/Handlers/ProductEventHandler.cs
@@ -0,0 +1,17 @@
+﻿using Commander.Playground.Tests.Contexts.Product.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commander.Playground.Tests
+{
+    public class ProductEventHandler : IEventHandler<ProductAddedEvent>
+    {
+        public async ValueTask Publish(ProductAddedEvent request)
+        {
+            await Task.CompletedTask.ConfigureAwait(false);
+        }
+    }
+}
